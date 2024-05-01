e081e069df1c3c11b6f47e089c3d42254fd5aeaf
huzaifanur <huzaifa98nur@gmail.com>
2024-05-01 08:08:59 +0000

    Add twitter api

--- go.mod
+++ go.mod
@@ -2,32 +2,4 @@ module x-stream
 
 go 1.22.2
 
-require (
-	github.com/gofiber/fiber/v2 v2.52.4
-	golang.ngrok.com/ngrok v1.9.1
-)
-
-require (
-	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/google/uuid v1.5.0 // indirect
-	github.com/inconshreveable/log15 v3.0.0-testing.3+incompatible // indirect
-	github.com/inconshreveable/log15/v3 v3.0.0-testing.5 // indirect
-	github.com/jpillora/backoff v1.0.0 // indirect
-	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	golang.ngrok.com/muxado/v2 v2.0.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,126 +1,82 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-const BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAKb6tQEAAAAA%2FKjz%2B9VxdlhhA8geqTiSxL5MV%2B4%3DlHATvPk2FpNp1FnqsHfQJ1SuKHESBG4caNWJAwVhlKAtgJQNEh"
+type TweetData struct {
+	Data struct {
+		ID                  string   `json:"id"`
+		EditHistoryTweetIDs []string `json:"edit_history_tweet_ids"`
+		Text                string   `json:"text"`
+	} `json:"data"`
+}
+
+func streamTwitterData(dataChan chan<- TweetData) error {
+	BEARER_TOKEN := os.Getenv("BEARER_TOKEN")
+	url := "https://api.twitter.com/2/tweets/search/stream"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	req.Header.Add("Authorization", "Bearer "+BEARER_TOKEN)
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer res.Body.Close()
+
+	reader := bufio.NewReader(res.Body)
+
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			log.Println(err)
+			break // Exit the loop on error
+		}
+
+		var tweet TweetData
+		err = json.Unmarshal(line, &tweet)
+		if err != nil {
+			log.Println(err)
+			continue // Skip on unmarshaling error
+		}
+
+		dataChan <- tweet
+	}
+	return nil
+}
 
 func main() {
-	app := fiber.New()
 
-	// Define the endpoint that calls Twitter's streaming API
-	app.Get("/stream-twitter", func(c *fiber.Ctx) error {
-		// Set up the request to Twitter's streaming API
-		url := "https://api.twitter.com/2/tweets/search/stream"
-		method := "GET"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-		client := &http.Client{}
-		req, err := http.NewRequest(method, url, nil)
+	dataChan := make(chan TweetData)
+
+	go func() {
+		err := streamTwitterData(dataChan)
 		if err != nil {
-			fmt.Println(err)
-			return err
+			log.Println(err)
 		}
-		bearerToken := BEARER_TOKEN
-		req.Header.Add("Authorization", "Bearer "+bearerToken)
+	}()
 
-		// Make the request to Twitter's API
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		defer res.Body.Close()
-
-		// Read the response body
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		// Log the response body
-		log.Println(string(body))
-
-		// Send the response back to the client
-		return c.SendString(string(body))
-	})
-
-	app.Post("/add-rules", func(c *fiber.Ctx) error {
-		url := "https://api.twitter.com/2/tweets/search/stream/rules"
-		method := "POST"
-
-		// Define the rules you want to add
-		rules := []byte(`{
-			"add": [
-				{"value": "dog has:images", "tag": "dog pictures"},
-				{"value": "cat has:images -grumpy"}
-			]
-		}`)
-
-		client := &http.Client{}
-		req, err := http.NewRequest(method, url, bytes.NewBuffer(rules))
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		bearerToken := BEARER_TOKEN // Replace with your actual bearer token
-		req.Header.Add("Authorization", "Bearer "+bearerToken)
-		req.Header.Add("Content-Type", "application/json")
-
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		defer res.Body.Close()
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		log.Println(string(body))
-		return c.SendString(string(body))
-	})
-
-	app.Get("/get-rules", func(c *fiber.Ctx) error {
-		url := "https://api.twitter.com/2/tweets/search/stream/rules"
-		method := "GET"
-
-		client := &http.Client{}
-		req, err := http.NewRequest(method, url, nil)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		bearerToken := BEARER_TOKEN // Replace with your actual bearer token
-		req.Header.Add("Authorization", "Bearer "+bearerToken)
-
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		defer res.Body.Close()
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		log.Println(string(body))
-		return c.SendString(string(body))
-	})
-
-	// Start the server on port 3000
-	log.Fatal(app.Listen(":3000"))
+	for tweet := range dataChan {
+		fmt.Printf("Tweet: %+v\n", tweet)
+	}
 }
